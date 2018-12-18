f51ecb6c68f8271b0c1c940bd053c044d809db88
Shivink <shiv@inkhive.com>
2018-12-18 14:25:24 +0530

    Gutenberg Ready, Minor Bug Fixes, Version Updated, Public Release v1.1.1

--- framework/admin_modules/admin_styles.php
+++ framework/admin_modules/admin_styles.php
@@ -3,6 +3,7 @@
  * Enqueue Scripts for Admin
  */
 function freak_custom_wp_admin_style() {
+    wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css' );
     wp_enqueue_style( 'freak-admin_css', get_template_directory_uri() . '/assets/css/admin.css' );
 }
 add_action( 'admin_enqueue_scripts', 'freak_custom_wp_admin_style' );
